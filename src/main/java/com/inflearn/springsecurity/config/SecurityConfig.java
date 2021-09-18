7ac24f11855800cf9882192476310f450214b891
HarilSong <songkg7@gmail.com>
2021-09-18 20:33:38 +0900

    Form login 인증

--- src/main/java/com/inflearn/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/inflearn/springsecurity/config/SecurityConfig.java
@@ -1,9 +1,17 @@
 package com.inflearn.springsecurity.config;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 //@Configuration
 @EnableWebSecurity
@@ -18,7 +26,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         // 인증 정책
         http
-                .formLogin();
+                .formLogin()
+//                .loginPage("/loginPage")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login")
+                .usernameParameter("userId") // Security 가 생성해주는 로그인 폼의 파라미터 값 설정
+                .passwordParameter("pwd")
+                .loginProcessingUrl("/login-proc") // Security 가 생성해주는 로그인 폼의 Action 값 설정
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request,
+                            HttpServletResponse response, Authentication authentication)
+                            throws IOException, ServletException {
+                        System.out.println("authentication" + authentication.getName());
+                        response.sendRedirect("/");
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest request,
+                            HttpServletResponse response, AuthenticationException exception)
+                            throws IOException, ServletException {
+                        System.out.println("exception" + exception.getMessage());
+                        response.sendRedirect("/login");
+                    }
+                })
+                .permitAll();
 
     }
 
