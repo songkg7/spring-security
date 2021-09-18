3b8a79b3ef807420dc1be42b47a0c12e738952ac
Haril Song <songkg7@gmail.com>
2021-09-19 00:13:31 +0900

    RememberMe 인증

--- src/main/java/com/inflearn/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/inflearn/springsecurity/config/SecurityConfig.java
@@ -5,20 +5,27 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 //@Configuration
+@Slf4j
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -41,7 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     public void onAuthenticationSuccess(HttpServletRequest request,
                             HttpServletResponse response, Authentication authentication)
                             throws IOException, ServletException {
-                        System.out.println("authentication" + authentication.getName());
+                        log.info("authentication = {}", authentication.getName());
                         response.sendRedirect("/");
                     }
                 })
@@ -50,11 +57,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     public void onAuthenticationFailure(HttpServletRequest request,
                             HttpServletResponse response, AuthenticationException exception)
                             throws IOException, ServletException {
-                        System.out.println("exception" + exception.getMessage());
+                        log.info("exception.getMessage() = {}", exception.getMessage());
                         response.sendRedirect("/login");
                     }
                 })
-                .permitAll();
+
+                // RememberMe 인증
+                .and()
+                .rememberMe()
+                .rememberMeParameter("remember") // 기본 파라미터명은 remember-me
+                .tokenValiditySeconds(3600)
+                .alwaysRemember(false)  // rememberMe 기능이 활성화되지 않아도 항상 실행
+                .userDetailsService(userDetailsService);
 
         // logout 설정 - Security 의 기본 로그아웃 정책은 Post 요청으로 처리된다.
         http
