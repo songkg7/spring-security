a6928101b2f031ecd6fc905929c556cc4ad146b9
Haril <songkg7@gmail.com>
2021-09-19 13:24:01 +0900

    동시 세션 제어, 세션 고정 보호, 세션 정책

--- src/main/java/com/inflearn/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/inflearn/springsecurity/config/SecurityConfig.java
@@ -1,31 +1,13 @@
 package com.inflearn.springsecurity.config;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
-//@Configuration
 @Slf4j
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -36,62 +18,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         // 인증 정책
         http
-                .formLogin()
-//                .loginPage("/loginPage")
-                .defaultSuccessUrl("/")
-                .failureUrl("/login")
-                .usernameParameter("userId") // Security 가 생성해주는 로그인 폼의 파라미터 값 설정
-                .passwordParameter("pwd")
-                .loginProcessingUrl("/login-proc") // Security 가 생성해주는 로그인 폼의 Action 값 설정
-                .successHandler(new AuthenticationSuccessHandler() {
-                    @Override
-                    public void onAuthenticationSuccess(HttpServletRequest request,
-                            HttpServletResponse response, Authentication authentication)
-                            throws IOException, ServletException {
-                        log.info("authentication = {}", authentication.getName());
-                        response.sendRedirect("/");
-                    }
-                })
-                .failureHandler(new AuthenticationFailureHandler() {
-                    @Override
-                    public void onAuthenticationFailure(HttpServletRequest request,
-                            HttpServletResponse response, AuthenticationException exception)
-                            throws IOException, ServletException {
-                        log.info("exception.getMessage() = {}", exception.getMessage());
-                        response.sendRedirect("/login");
-                    }
-                })
+                .formLogin();
 
-                // RememberMe 인증
-                .and()
-                .rememberMe()
-                .rememberMeParameter("remember") // 기본 파라미터명은 remember-me
-                .tokenValiditySeconds(3600)
-                .alwaysRemember(false)  // rememberMe 기능이 활성화되지 않아도 항상 실행
-                .userDetailsService(userDetailsService);
-
-        // logout 설정 - Security 의 기본 로그아웃 정책은 Post 요청으로 처리된다.
+        // 동시세션제어
         http
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login")
-                .addLogoutHandler(new LogoutHandler() {
-                    @Override
-                    public void logout(HttpServletRequest request, HttpServletResponse response,
-                            Authentication authentication) {
-                        HttpSession session = request.getSession();
-                        session.invalidate();
-                    }
-                })
-                .logoutSuccessHandler(new LogoutSuccessHandler() {
-                    @Override
-                    public void onLogoutSuccess(HttpServletRequest request,
-                            HttpServletResponse response, Authentication authentication)
-                            throws IOException, ServletException {
-                        response.sendRedirect("/login");
-                    }
-                })
-                .deleteCookies("remember-me");
+                .sessionManagement()
+                // .sessionFixation().none() // 세션 고정 보호 해제, 보안에 취약해지기 때문에 기본값은 changeSessionId 로 되어 있다.
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(true);  // default false
 
     }
 
