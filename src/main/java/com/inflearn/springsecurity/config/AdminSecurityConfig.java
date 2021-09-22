ab276495ea664d52bc7faba358bb74c7803c6115
Haril <songkg7@gmail.com>
2021-09-22 11:51:15 +0900

    필터 초기화와 다중 보안 설정

--- src/main/java/com/inflearn/springsecurity/config/AdminSecurityConfig.java
+++ src/main/java/com/inflearn/springsecurity/config/AdminSecurityConfig.java
@@ -1,22 +1,25 @@
 package com.inflearn.springsecurity.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Order(1)
+@Slf4j
+@Order(0)
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class AdminSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http
+                .antMatcher("/admin/**")
                 .authorizeRequests()
-                .anyRequest().permitAll()
+                .anyRequest().authenticated()
                 .and()
-                .formLogin();
+                .httpBasic();
 
     }
 
